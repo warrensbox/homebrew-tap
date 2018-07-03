6c337cae939c7dc5839a0c7f8b6f40ae9febf08d
Warren Veerasingam <warren.veerasingam@gmail.com>
2018-07-02 20:42:06 -0500

    Brew formula update for terragrunt-switcher version 0.1.67

--- Formula/tgshift.rb
+++ Formula/tgshift.rb
@@ -1,9 +1,9 @@
 class Tgshift < Formula
   desc "The tgshift command lets you switch between terragrunt versions."
   homepage "https://warrensbox.github.io/terragrunt-switcher"
-  url "https://github.com/warrensbox/terragrunt-switcher/releases/download/0.1.41/terragrunt-switcher_0.1.41_darwin_amd64.tar.gz"
-  version "0.1.41"
-  sha256 "94bdeae18a0014b61501f3bb277a151e7147454addcee942f0c34dea34fd6474"
+  url "https://github.com/warrensbox/terragrunt-switcher/releases/download/0.1.67/terragrunt-switcher_0.1.67_darwin_amd64.tar.gz"
+  version "0.1.67"
+  sha256 "c97b949363bdfab95751aff1f6e69d4b2a3726a2514e561c743c1e5f7df9b822"
 
   def install
     bin.install "tgshift"
