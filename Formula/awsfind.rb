c40fd0c80c295c7cf197f94377da778ede6f253a
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-02-03 14:55:59 -0600

    Brew formula update for aws-find version 0.1.455

--- Formula/awsfind.rb
+++ Formula/awsfind.rb
@@ -1,9 +1,9 @@
 class Awsfind < Formula
   desc "The awsfind command lets you switch between terragrunt versions."
   homepage "https://warrensbox.github.io/aws-find"
-  url "https://github.com/warrensbox/aws-find/releases/download/0.1.448/aws-find_0.1.448_darwin_amd64.tar.gz"
-  version "0.1.448"
-  sha256 "18777a6394eebbfa000e51b551c637cc0e37f1f17a3c602d9e7bd179bd84b641"
+  url "https://github.com/warrensbox/aws-find/releases/download/0.1.455/aws-find_0.1.455_darwin_amd64.tar.gz"
+  version "0.1.455"
+  sha256 "2a2173cbb2c97d5c36b15ae93a7319f5943c3c15c372f28c05112c40b07fa61e"
 
   def install
     bin.install "awsfind"
