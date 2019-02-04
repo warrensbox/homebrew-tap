e30d6b86e6db9c452ab0815c3f363aa5774e4172
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-02-03 18:33:24 -0600

    Brew formula update for github-appinstaller version 0.1.65

--- Formula/appinstall.rb
+++ Formula/appinstall.rb
@@ -1,9 +1,9 @@
 class Appinstall < Formula
   desc "The appinstall command lets you install binary application from github."
   homepage "https://warrensbox.github.io/github-appinstall"
-  url "https://github.com/warrensbox/github-appinstaller/releases/download/0.1.58/github-appinstaller_0.1.58_darwin_amd64.tar.gz"
-  version "0.1.58"
-  sha256 "b215c7acab5c3c6e73c6fa4dafc2b713cace118d0087df303b0298b358649151"
+  url "https://github.com/warrensbox/github-appinstaller/releases/download/0.1.65/github-appinstaller_0.1.65_darwin_amd64.tar.gz"
+  version "0.1.65"
+  sha256 "50798dd4a106a3b274ae33c156b341c5bcca925909314edd3f53da59cefe8124"
 
   def install
     bin.install "appinstall"
