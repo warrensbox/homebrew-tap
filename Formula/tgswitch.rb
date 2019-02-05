65b172d4e812cb68615494843899494088675fe9
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-02-04 22:27:10 -0600

    Brew formula update for terragrunt-switcher version 0.2.210

--- Formula/tgswitch.rb
+++ Formula/tgswitch.rb
@@ -1,9 +1,9 @@
 class Tgswitch < Formula
   desc "The tgswitch command lets you switch between terragrunt versions."
   homepage "https://warrensbox.github.io/terragrunt-switcher"
-  url "https://github.com/warrensbox/terragrunt-switcher/releases/download/0.2.206/terragrunt-switcher_0.2.206_darwin_amd64.tar.gz"
-  version "0.2.206"
-  sha256 "810b0d2c7ac143c9f397ef75d42d60f2b97c4da90faebdcb5309c1f9070442d4"
+  url "https://github.com/warrensbox/terragrunt-switcher/releases/download/0.2.210/terragrunt-switcher_0.2.210_darwin_amd64.tar.gz"
+  version "0.2.210"
+  sha256 "4144281084d5388510cd3c0ed90663d7187484fa3292231e747c5ce0f7a3e3e8"
 
   def install
     bin.install "tgswitch"
