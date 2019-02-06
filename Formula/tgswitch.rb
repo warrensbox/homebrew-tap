c6352e8a0c3f28a9b6712d5a8a375d1972354a7a
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-02-06 08:22:19 -0600

    Brew formula update for tgswitch version 0.3.285

--- Formula/tgswitch.rb
+++ Formula/tgswitch.rb
@@ -1,9 +1,9 @@
 class Tgswitch < Formula
   desc "The tgswitch command lets you switch between terragrunt versions."
   homepage "https://warrensbox.github.io/tgswitch"
-  url "https://github.com/warrensbox/tgswitch/releases/download/0.3.282/tgswitch_0.3.282_darwin_amd64.tar.gz"
-  version "0.3.282"
-  sha256 "37dce35534305f92f32478161efe628bc32ee1a7f60244df5ff5c18fa2631acb"
+  url "https://github.com/warrensbox/tgswitch/releases/download/0.3.285/tgswitch_0.3.285_darwin_amd64.tar.gz"
+  version "0.3.285"
+  sha256 "1347c0d725d05880761ccafa753bd515bb6976f5c4352eac4156ed4e3826d719"
 
   def install
     bin.install "tgswitch"
