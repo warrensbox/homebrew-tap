bcc9ab1a55195385814e3578fd0360a7c2f6adee
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-02-13 14:41:35 -0500

    Brew formula update for hubapp version 0.2.96

--- Formula/hubapp.rb
+++ Formula/hubapp.rb
@@ -1,9 +1,9 @@
 class Hubapp < Formula
   desc "The hubapp command lets you install binary application from github."
   homepage "https://warrensbox.github.io/hubapp"
-  url "https://github.com/warrensbox/hubapp/releases/download/0.2.90/hubapp_0.2.90_darwin_amd64.tar.gz"
-  version "0.2.90"
-  sha256 "b86b5444f94d557009b4e40a456630b74c7fa04cd1e451af5c868b41c9b95486"
+  url "https://github.com/warrensbox/hubapp/releases/download/0.2.96/hubapp_0.2.96_darwin_amd64.tar.gz"
+  version "0.2.96"
+  sha256 "5541ffe519c90660cdd4ea1e186f4e215f8a1760f5842d3beed824d596b81e41"
 
   def install
     bin.install "hubapp"
