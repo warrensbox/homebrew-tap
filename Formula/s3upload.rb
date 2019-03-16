4ed319d8f78e3b28e63a8fc35371f438f1668c4d
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-03-15 19:46:12 -0500

    Brew formula update for s3upload version 0.1.40

--- Formula/s3upload.rb
+++ Formula/s3upload.rb
@@ -1,9 +1,9 @@
 class S3upload < Formula
   desc "The s3upload command lets you upload files to s3"
   homepage "https://warrensbox.github.io/s3upload"
-  url "https://github.com/warrensbox/s3upload/releases/download/0.1.37/s3upload_0.1.37_darwin_amd64.tar.gz"
-  version "0.1.37"
-  sha256 "d3e2aae7cf4dd1609552774a4e836793b1661625dd0e904e38e268440f66f3da"
+  url "https://github.com/warrensbox/s3upload/releases/download/0.1.40/s3upload_0.1.40_darwin_amd64.tar.gz"
+  version "0.1.40"
+  sha256 "fce62783057d790d115f89ae6e687073ee0bbded65ca0bb3aeb9d80cbf4538e0"
 
   def install
     bin.install "s3upload"
