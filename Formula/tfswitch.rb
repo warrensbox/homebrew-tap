71ed22d3921cc24ca257d2f4180251b0194ddbda
Warren Veerasingam <warren.veerasingam@gmail.com>
2019-05-22 14:35:48 -0500

    Brew formula update for terraform-switcher version 0.6.696

--- Formula/tfswitch.rb
+++ Formula/tfswitch.rb
@@ -1,9 +1,11 @@
 class Tfswitch < Formula
   desc "The tfswitch command lets you switch between terraform versions."
   homepage "https://warrensbox.github.io/terraform-switcher"
-  url "https://github.com/warrensbox/terraform-switcher/releases/download/0.5.642/terraform-switcher_0.5.642_darwin_amd64.tar.gz"
-  version "0.5.642"
-  sha256 "045434789707e4252cc588bc343d76c7a442d9cb6d716102f61f3f79ce0729da"
+  url "https://github.com/warrensbox/terraform-switcher/releases/download/0.6.696/terraform-switcher_0.6.696_darwin_amd64.tar.gz"
+  version "0.6.696"
+  sha256 "a4fada902dfda47b03950a6ba0dd2048d4b964cb4082c115adf3281f6901267e"
+  
+  conflicts_with "terraform"
 
   def install
     bin.install "tfswitch"
